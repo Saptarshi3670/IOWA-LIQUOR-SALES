d8550817d695d28e0352c19f7f6b18f62ff1b68d
kj251295 <57553301+kj251295@users.noreply.github.com>
2020-05-02 15:07:35 -0500

    Updates Plumber model runner for deployment
    Necessary updates made to the code.

--- plumber_model.R
+++ plumber_model.R
@@ -8,7 +8,7 @@
 # Find out more about building APIs with Plumber here:
 #
 #    https://www.rplumber.io/
-#
+
 
 library(plumber)
 
@@ -24,9 +24,6 @@ function(msg=""){
   list(msg = paste0("The message is: '", msg, "'"))
 }
 
-#loc <- "C:/Users/kinja/OneDrive/Desktop/All/2. SEM2/DPA/Descision_Tree/DecisionTree/DecisionTree_Plumber"
-#load(paste(loc, "/train.rdata", sep = ""))
-#load(paste(loc, "/test.rdata", sep = ""))
 
 list_of_packages <- c("tidyverse", "plumber", "caret")
 new.packages <- list_of_packages[!(list_of_packages %in% installed.packages()[,"Package"])]
@@ -39,21 +36,6 @@ if (length(new.packages)) {
 path <- "C:/Users/kinja/OneDrive/Desktop/All/2. SEM2/DPA/Descision_Tree/DecisionTree/DecisionTree_Plumber/decisiontree.rdata"
 model1 <- load(path) 
 
-model_pred<- function(model1, Bottle_vol, State.Bottle.Cost, Bottles.Sold){
-  
-  log_Bottle_vol <- log(Bottle_vol)
-  log_State.Bottle.Cost <- log(State.Bottle.Cost)
-  log_Bottles.Sold <- log(Bottles.Sold)
-  
-  input_data <- data.frame(log_Bottle_vol,
-                           log_State.Bottle.Cost,
-                           log_Bottles.Sold)
-  
-  pred <<- predict(get(model1), input_data, type = "class")
-  return(pred)
-  
-}
-
 
 #' Model adding Volume Sold, Number of bottles, Sales in Dollars per pack. 
 #' predict the probability of an product being potential seller(y/n).
@@ -61,23 +43,22 @@ model_pred<- function(model1, Bottle_vol, State.Bottle.Cost, Bottles.Sold){
 #' @param Bottle_vol:numeric Volume in litres.
 #' @param State.Bottle.Cost:numeric Costo of bottles
 #' @param Bottles.Sold:numeric Number of bottles per pack 
-#' @param Brand_Alcoholtype Give the brand alcohol combination
-#' @post /model_pred
+#' @post /model_prediction
 #' @html
 #' @response Return prediction
  
 predict_potential_brand <- function(Bottle_vol, State.Bottle.Cost, Bottles.Sold){
   
-  log_Bottle_vol <- log(Bottle_vol)
-  log_State.Bottle.Cost <- log(State.Bottle.Cost)
-  log_Bottles.Sold <- log(Bottles.Sold)
+  
+  log_Bottle_vol <- log(as.numeric(Bottle_vol))
+  log_State.Bottle.Cost <- log(as.numeric(State.Bottle.Cost))
+  log_Bottles.Sold <- log(as.numeric(Bottles.Sold))
   
   input_data <- data.frame(log_Bottle_vol,
                            log_State.Bottle.Cost,
                            log_Bottles.Sold)
   
-  predict(get(model1), input_data, type = "class")
+  op <- predict(get(model1), input_data, type = "class")
+  op <- as.character(op[[1]])
   
 } 
-
-
